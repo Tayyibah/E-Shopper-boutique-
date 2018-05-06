a28b2c6f0151933a9c33ac647fd211c29df94f70
Tayyibah Alauddin <tayyibahalauddin@gmail.com>
2018-05-06 15:56:05 +0500

    db updated

--- EAD_Project/DAL/DBHelper.cs
+++ EAD_Project/DAL/DBHelper.cs
@@ -10,12 +10,13 @@ namespace EAD_Project.DAL
 {
     internal class DBHelper : IDisposable
     {
-        String _connStr = @"data source=dff3ef06-5cc4-4b00-9f7b-a7ab00751cbc.sqlserver.sequelizer.com;initial catalog=dbdff3ef065cc44b009f7ba7ab00751cbc;user id=newhamupbintczqz;password=zXE3MMu2q2BswuKEPCngvtsUPygAK8qHkGYFGgEJZ4N3GVHAdG7XwRefDTmoF2pr;MultipleActiveResultSets=True;App=EntityFramework&quot";
-       // String _connStr = @"data source=375b2c8c-b6ff-48af-8185-a7aa00d72b05.sqlserver.sequelizer.com;initial catalog=db375b2c8cb6ff48af8185a7aa00d72b05;user id=dtffwtqynzwfmlmr;password=zXE3MMu2q2BswuKEPCngvtsUPygAK8qHkGYFGgEJZ4N3GVHAdG7XwRefDTmoF2pr;MultipleActiveResultSets=True;App=EntityFramework&quot;";
-       // String _connStr = @"Data Source=DESKTOP-AQ23G94\SQLEXPRESS;Initial Catalog=EShopper;Integrated Security=True";
+        String _connStr = @"data source=5bca5db0-8862-485d-b966-a8d800ac7903.sqlserver.sequelizer.com;initial catalog=db5bca5db08862485db966a8d800ac7903;user id=sgbsonlsbqggdoqh;password=tyS8pezVbi2rNcQ8xvpSqDxhcFGoUYjzfQsk8G2MWrSzMkfb6pZTs5aWbK5REkbk;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+        //String _connStr = @"data source=dff3ef06-5cc4-4b00-9f7b-a7ab00751cbc.sqlserver.sequelizer.com;initial catalog=dbdff3ef065cc44b009f7ba7ab00751cbc;user id=newhamupbintczqz;password=zXE3MMu2q2BswuKEPCngvtsUPygAK8qHkGYFGgEJZ4N3GVHAdG7XwRefDTmoF2pr;MultipleActiveResultSets=True;App=EntityFramework&quot";
+        // String _connStr = @"data source=375b2c8c-b6ff-48af-8185-a7aa00d72b05.sqlserver.sequelizer.com;initial catalog=db375b2c8cb6ff48af8185a7aa00d72b05;user id=dtffwtqynzwfmlmr;password=zXE3MMu2q2BswuKEPCngvtsUPygAK8qHkGYFGgEJZ4N3GVHAdG7XwRefDTmoF2pr;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+        // String _connStr = @"Data Source=DESKTOP-AQ23G94\SQLEXPRESS;Initial Catalog=EShopper;Integrated Security=True";
         //String _connStr = System.Configuration.ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
-      //  String _connStr = @"data source = dff3ef06-5cc4-4b00-9f7b-a7ab00751cbc.sqlserver.sequelizer.com;initial catalog = dbdff3ef065cc44b009f7ba7ab00751cbc; user id = newhamupbintczqz; password=6uiQuRHrN3gLQVGw44HQ6mYrtUUNEVtQ8SyTMayWHJKfvk3qVjYjv67TDZvG64TZ;MultipleActiveResultSets=True";
-      // String _connStr = @"Data Source = DESKTOP - 93644DJ\SQLEXPRESS;Initial Catalog = Shopping_DB; Integrated Security = True;";
+        //  String _connStr = @"data source = dff3ef06-5cc4-4b00-9f7b-a7ab00751cbc.sqlserver.sequelizer.com;initial catalog = dbdff3ef065cc44b009f7ba7ab00751cbc; user id = newhamupbintczqz; password=6uiQuRHrN3gLQVGw44HQ6mYrtUUNEVtQ8SyTMayWHJKfvk3qVjYjv67TDZvG64TZ;MultipleActiveResultSets=True";
+        // String _connStr = @"Data Source = DESKTOP - 93644DJ\SQLEXPRESS;Initial Catalog = Shopping_DB; Integrated Security = True;";
         SqlConnection _conn = null;
             public DBHelper()
             {
